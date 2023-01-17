74333db4652df03ea02386161980005ff7e1accd
Tiago Efrens <98785464+tiagoefrens@users.noreply.github.com>
2023-01-17 20:14:39 -0300

    switch

---
+++ aula12/ex13.js
@@ -0,0 +1,32 @@
+var agora = new Date()
+var  diaSem = agora.getDay()
+console.log(diaSem)
+
+switch(diaSem){
+    case 0 : 
+        console.log('Domingo')
+        break
+    case 1 :
+        console.log('Segunda-Feira')
+        break
+    case 2 :
+            console.log('Quarta-Feira')
+            break
+    case 3 :
+                console.log('Quinta-Feira')
+                break
+  case 4 :
+                console.log('Sexta-Feira')
+                break
+    case 5 :
+                console.log('Sabado')
+                break
+
+    default:
+        console.log('Dia inválido!')
+        break
+
+
+
+}
+            
