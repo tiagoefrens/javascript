bf1fd45fcf61fe15df81ea08717f1bf7e43321fa
Tiago Efrens <98785464+tiagoefrens@users.noreply.github.com>
2023-01-18 20:35:01 -0300

    exercicios js

---
+++ aula12ex/ex014/script.js
@@ -0,0 +1,24 @@
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora =19//data.getHours()
+    msg.innerHTML = `Agora são ${hora} Horas.`
+
+
+    if (hora >= 0 && hora < 12){
+        //Bom dia
+        img.src = 'fotomanha.png'
+        document.body.style.background = '#798661'
+    } else if (hora >= 12 && hora <= 18){
+         //Boa tarde
+        img.src = 'fototarde.png'
+        document.body.style.background = '#FBB59C'
+    } else {
+        //Boa noite  
+        img.src = 'fotonoite.png' 
+        document.body.style.background = '#382D4A'
+        
+    }
+}
+
