0cfa68e99588fba3260eef43a407e5dece9e45fd
Tiago Efrens <98785464+tiagoefrens@users.noreply.github.com>
2023-01-19 20:16:41 -0300

    exercicios part 3

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -2,7 +2,7 @@ function carregar(){
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var data = new Date()
-    var hora =19//data.getHours()
+    var hora =//data.getHours()
     msg.innerHTML = `Agora são ${hora} Horas.`
 
 
---
+++ aula12ex/ex015/script.js
@@ -0,0 +1,50 @@
+function verificar(){
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.getElementById('res')
+
+    if(fano.value.length == 0 || fano.value > ano){
+        window.alert('[ERRO] Verifique os dados e tente novamente! ')
+    }else {
+       var fsex = document.getElementsByName('radsex')
+       var idade = ano - Number(fano.value)
+       var genero = ''
+       var img = document.createElement('img')
+       img.setAttribute('id', 'foto')
+       if(fsex[0].checked){
+            genero = 'Homem'
+            if(idade>=0 && idade <10){
+                //criança
+                img.setAttribute('src', 'hbebe.png')
+            }else if(idade <21 ){
+                //jovem
+                img.setAttribute('src', 'hadolescente.png')
+            }else if (idade < 50 ){
+                //adulto
+                img.setAttribute('src', 'hadulto.png')
+            }else{
+                //idoso
+                img.setAttribute('src', 'hvelho.png')
+            }
+       }else if (fsex[1].checked){
+            genero = 'Mulher'
+            if(idade>=0 && idade <10){
+                //criança
+                img.setAttribute('src', 'mbebe.png')
+            }else if(idade <21 ){
+                //jovem
+                img.setAttribute('src', 'madolescente.png')
+            }else if (idade < 50 ){
+                //adulto
+                img.setAttribute('src', 'madulto.png')
+            }else{
+                //idoso
+                img.setAttribute('src', 'mvelha.png')
+            }
+       }
+       res.style.textAlign = 'center'
+       res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+       res.appendChild(img)
+    }
+}
